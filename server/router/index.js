ce0b9f074ae0a0b550461628a5407a044c968cac
YangWenlong123 <496192903@qq.com>
2020-11-13 14:31:23 +0800

    feat: 脚手架配置调整

--- server/router/index.js
+++ server/router/index.js
@@ -3,17 +3,17 @@
  * @Author: along
  * @Date: 2019-11-12
  */
-const router = require('koa-router')();
+// const router = require('koa-router')();
 
-const indexPage = {
-    async indexPage ( ctx ) {
-        const title = 'admin page'
-        await ctx.render('index', {
-            title
-        })
-    }
-}
-const home = router.get('/', indexPage.indexPage)
-router.use('/', home.routes(), home.allowedMethods());
+// const indexPage = {
+//     async indexPage ( ctx ) {
+//         const title = 'admin page'
+//         await ctx.render('index', {
+//             title
+//         })
+//     }
+// }
+// const home = router.get('/', indexPage.indexPage)
+// router.use('/', home.routes(), home.allowedMethods());
 
-module.exports = router;
+// module.exports = router;
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="home">
+        aa12
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+    data() {
+        return {
+            info: "",
+        };
+    },
+    computed: {
+
+    },
+    components: {
+    },
+    methods: {
+
+    }
+};
+</script>
